8b5b7d4473ad419f7afb10098f55788555e33533
TacoTruckMan777 <jcruz91011@gmail.com>
2025-09-18 09:21:12 -0400

    Update test.py

--- test.py
+++ test.py
@@ -1,6 +1,13 @@
-def not_movies(homework):
-    if (homework == False):
-        print("Going to the Movies")
-    else:
-        print("I hate Russain")
-not_movies(homework=True)
+#def not_movies(homework):
+    #if (homework == False):
+        #print("Going to the Movies")
+    #else:
+        #print("I hate Russain")
+#not_movies(homework=True)
+#values = [1,2.23,5,7,2,30,15]
+#print(values)
+#for i in values:
+    #print(i)
+#print(values[0])
+#print(values[6])
+
